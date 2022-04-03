766fdfadb4a9a5515c72c50ec72188d9d81d4551
EC2 Default User <alpaca080013@gmail.com>
2022-04-03 13:42:18 +0000

    Update

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ItemsController < ApplicationController
   def index
-    @items=Item.page(params[:page]).per(5)
+    @items=Item.page(params[:page]).per(3)
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+
+  
+    get 'items', to:'public/items#index'
+    get 'items/:id', to:'public/items#show'
+  
+    get 'customers', to:'public/customers#show'
+    get 'customers/edit', to:'public/customers#edit'
+    patch 'customers', to:'public/customers#update'
+    get 'customers/unsubscribe', to:'public/customers#unsubscribe'
+    patch 'customers/withdrawal', to:'public/customers#withdrawal'
+
   root to:'public/homes#top'
-  
-  
-  
+
+
+
     get '/about', to:'public/homes#about'
 
   namespace :admin do
