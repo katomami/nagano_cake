99d130352d0ed6082337dc0d7f899733b9e05501
katomami <alpaca080013@gmail.com>
2022-04-24 09:37:02 +0000

    Update

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,24 +1,31 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
     order_detail=OrderDetail.find(params[:id])
-    if order_detail_params[:making_status]=="in_production"
-      order_detail.update(making_status: "under_construction")
-    end
-    
+
+order_detail.update(making_status: order_detail_params[:making_status])
     order=order_detail.order
     cake=0
-    
+    cookie=0
     order.order_details.each do |good|
       if good.making_status=="in_production"
         cake+=1
       end
-      end
+    end
       if cake>=1
-        order.update(status: "under_construction")
+        order.update(status:2)
         cake=0
       end
-    order_detail.update(making_status: order_detail_params[:making_status])
-    redirect_to admin_order_path(order_detail)
+    order.order_details.each do |apple|
+      if apple.making_status=="production_completed"
+        cookie+=1
+      end
+    end
+      if cookie==order.order_details.count
+        order.update(status:3)
+        cookie=0
+      end
+
+    redirect_to admin_order_path(order.id)
   end
    private
   def order_detail_params
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,7 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order=Order.find(params[:id])
+    @order_details=@order.order_details
   end
   def update
     order=Order.find(params[:id])
